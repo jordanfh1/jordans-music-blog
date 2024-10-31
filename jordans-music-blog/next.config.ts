7ed1cd3691bf8ba3e89101290412d3722ab84a96
Jordan <jordanfhoughton@gmail.com>
2024-10-31 17:10:08 +0000

    blog page styling

--- jordans-music-blog/next.config.ts
+++ jordans-music-blog/next.config.ts
@@ -4,4 +4,18 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '/wikipedia/en/3/3a/The_Sound_of_Sonny.jpg',
+      },
+    ],
+  },
+}
+
 export default nextConfig;
+
